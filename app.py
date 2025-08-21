272c4de697e542a20990dcacee3b4e50a94acc8d
Aman kumar <aman1782003@gmail.com>
2025-08-21 21:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import os
 st.set_page_config(page_title="Water Pollutants Predictor", layout="centered")
 
 # --- Google Drive links (replace with your actual file IDs) ---
-MODEL_URL = "https://drive.google.com/file/d/1FwzNKS3WvLOt0a-Xe7EgnOB8B4yN2CTr/view?usp=drive_link"
-COLS_URL = "https://drive.google.com/file/d/11VMiho6ikieZ_BgQZxaruwHxqFlMykcs/view?usp=drive_link"
+MODEL_URL = "https://drive.google.com/uc?id=1FwzNKS3WvLOt0a-Xe7EgnOB8B4yN2CTr"
+COLS_URL = "https://drive.google.com/uc?id=11VMiho6ikieZ_BgQZxaruwHxqFlMykcs"
 
 MODEL_PATH = "pollution_model.pkl"
 COLS_PATH = "model_columns.pkl"
@@ -19,7 +19,7 @@ COLS_PATH = "model_columns.pkl"
 def download_file(url, output, min_size=50000):
     if not os.path.exists(output):
         st.write(f"📥 Downloading {output} ...")
-        gdown.download(url, output, quiet=False)
+        gdown.download(url, output, quiet=False, fuzzy=True)
     if os.path.exists(output) and os.path.getsize(output) < min_size:
         st.error(f"❌ {output} file seems corrupted or not downloaded properly!")
         st.stop()
@@ -103,7 +103,6 @@ if st.button('Predict'):
                     st.write(f"🔴 **{p}** is too HIGH: {actual_value:.2f} mg/L (should be ≤ {safe_limits[p]} mg/L).")
 
             st.info("💡 To make the water safe, pollutants must be within safe limits.")
-
 
 
 
